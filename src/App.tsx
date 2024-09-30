d69d31325b80cc7e0221272a8eac07fffbd3eeef
kngval <bkvalteocs@gmail.com>
2024-09-30 16:17:43 +0800

    signup route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Login from "./pages/Login"
+import Signup from "./pages/Signup"
 
 function App(){
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/signup" element={<Signup />}/>
         </Routes>
         
       </BrowserRouter>
