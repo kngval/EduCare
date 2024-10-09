3d1cbd861eea62966412b5a5cd0bd76a11eeba7d
kngval <bkvalteocs@gmail.com>
2024-10-09 19:17:08 +0800

    conditional rendering

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,17 @@ import Signup from "./pages/Signup"
 import { useSelector } from "react-redux"
 import { RootState } from "./redux/store"
 import Dashboard from "./pages/Dashboard"
+import Navbar from "./components/Navbar"
+import Sections from "./pages/Sections"
+import Grades from "./pages/Grades"
+import Account from "./pages/Account"
 
 function App() {
   const token = useSelector((state: RootState) => state.authReducer.token);
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           {/*AUTH CHECK ROUTES*/}
           <Route path="/" element={token ? <Navigate to="/dashboard" /> : <Navigate to="/login" />} />
@@ -18,6 +23,10 @@ function App() {
 
           {/* Main Routes */}
           <Route path="/dashboard" element={token ? <Dashboard /> : <Navigate to="/login"/>}/>
+
+          <Route path="/sections" element={token ? <Sections /> : <Navigate to="/login" />}/>
+          <Route path="/grades" element={token ? <Grades /> : <Navigate to="/login" />}/>
+          <Route path="/account" element={token ? <Account /> : <Navigate to="/login" />}/>
         </Routes>
 
       </BrowserRouter>
