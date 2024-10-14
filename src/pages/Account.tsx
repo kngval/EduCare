b1e17cd26171f1e35a7b98be8c6fee4b58143330
kngval <bkvalteocs@gmail.com>
2024-10-15 01:00:54 +0800

    account tab

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,7 +1,20 @@
 
-function Account(){
-  return(
-  <div>Account</div>
+function Account() {
+  return (
+    <div className="grow">
+      <div className="header h-[300px] w-full border-2 border-white">
+
+      </div>
+
+      <div className="userInfo-container relative  grid justify-items-center">
+        <div className="pfp z-0 absolute -top-28 rounded-full bg-customLightBlue w-[200px] h-[200px]"></div>
+
+        <div className="w-[90%] bg-customBlue2">
+
+    dqiwjdqio
+        </div>
+      </div>
+    </div>
   )
 }
 export default Account;
