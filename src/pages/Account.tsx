09890fa433cee09bd5df8dc926f1c81089ab51d3
kngval <bkvalteocs@gmail.com>
2024-10-15 17:19:18 +0800

    structure

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,19 +1,13 @@
 
 function Account() {
   return (
-    <div className="grow">
-      <div className="header h-[300px] w-full border-2 border-white">
+    <div className="grow flex justify-center p-12">
+      
+    <div className="userInfo-container bg-customBlue2 rounded-xl p-4 w-[90%]">
+      
 
-      </div>
+    </div>
 
-      <div className="userInfo-container relative  grid justify-items-center">
-        <div className="pfp z-0 absolute -top-28 rounded-full bg-customLightBlue w-[200px] h-[200px]"></div>
-
-        <div className="w-[90%] bg-customBlue2">
-
-    dqiwjdqio
-        </div>
-      </div>
     </div>
   )
 }
