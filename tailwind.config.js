1c4f02985cc114a95ca2de254ee0fd1c9295b612
kngval <bkvalteocs@gmail.com>
2024-10-26 00:49:35 +0800

    added new blue colorscheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,18 +7,21 @@ export default {
         customBlack: "#8C89B4",
         customBlue: "#141332",
         customBlue2: "#1D1D41",
+        customBlue3: "#3A3A65",
         customLightBlue:"#6359E9",
         customPlaceholder: "#27264E"
       },
       backgroundColor: {
         customBlue: "#141332",
         customBlue2: "#1D1D41",
+        customBlue3: "#3A3A65",
         customLightBlue:"#6359E9",
         customPlaceholder: "#27264E"
       },
       borderColor : {
         customBlue: "#141332",
         customBlue2: "#1D1D41",
+        customBlue3: "#3A3A65",
         customLightBlue:"#6359E9",
         customPlaceholder: "#27264E"
       }
