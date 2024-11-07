488d53aed728f21777d1f388f6c087e4aaa36d71
kngval <bkvalteocs@gmail.com>
2024-11-07 14:10:16 +0800

    Admin Only Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
-import Login from "./pages/Login"
-import Signup from "./pages/Signup"
 import { useSelector } from "react-redux"
 import { RootState } from "./redux/store"
+//Pages
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
 import Dashboard from "./pages/Dashboard"
 import Navbar from "./components/Navbar"
 import Sections from "./pages/Sections"
 import Grades from "./pages/Grades"
 import Account from "./pages/Account"
+import Admin from "./pages/Admin"
 
 function App() {
   const token = useSelector((state: RootState) => state.authReducer.token);
@@ -27,6 +29,9 @@ function App() {
           <Route path="/sections" element={token ? <Sections /> : <Navigate to="/login" />} />
           <Route path="/grades" element={token ? <Grades /> : <Navigate to="/login" />} />
           <Route path="/account" element={token ? <Account /> : <Navigate to="/login" />} />
+          {/* Admin Routes */}
+
+          <Route path="/admin" element={token ? <Admin /> : <Navigate to="/login" />} />
         </Routes>
 
       </BrowserRouter>
