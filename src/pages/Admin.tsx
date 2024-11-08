b21f0fcbe66ac75133efb2e46ed27be862a81fa8
kngval <bkvalteocs@gmail.com>
2024-11-08 22:10:00 +0800

    generate code

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,8 +1,23 @@
 
-function Admin(){
+function Admin() {
 
-  return(
-    <div>Admin Only !!</div>
+  return (
+    <div className="grow mb-28 p-12">
+      <div className="grid">
+
+        <div className="flex justify-end">
+          <div className="bg-customLightBlue w-[200px] flex justify-center items-center gap-3 text-sm  py-2 rounded-lg">
+            <svg className="w-[15px]" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#ffffff"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <title></title> <g id="Complete"> <g data-name="add" id="add-2"> <g> <line fill="none" stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x1="12" x2="12" y1="19" y2="5"></line> <line fill="none" stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x1="5" x2="19" y1="12" y2="12"></line> </g> </g> </g> </g></svg>
+            <button>Generate Auth Code</button>
+          </div>
+        </div>
+      
+        {/* Display Codes here */}
+        <div>
+           
+        </div>
+      </div>
+    </div>
   )
 }
 
