377361671e2e2485d6101740c952da7325b3e1ae
kngval <bkvalteocs@gmail.com>
2024-11-27 21:22:38 +0800

    Room route

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
-import { useSelector } from "react-redux"
-import { RootState } from "./redux/store"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "./redux/store";
 //Pages
-import Login from "./pages/Login"
-import Signup from "./pages/Signup"
-import Dashboard from "./pages/Dashboard"
-import Navbar from "./components/Navbar"
-import Sections from "./pages/Sections"
-import Grades from "./pages/Grades"
-import Account from "./pages/Account"
-import Admin from "./pages/Admin"
-
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import Navbar from "./components/Navbar";
+import Rooms from "./pages/Rooms";
+import Grades from "./pages/Grades";
+import Account from "./pages/Account";
+import Admin from "./pages/Admin";
 function App() {
   const token = useSelector((state: RootState) => state.authReducer.token);
   return (
@@ -19,24 +18,49 @@ function App() {
         <Navbar />
         <Routes>
           {/*AUTH CHECK ROUTES*/}
-          <Route path="/" element={token ? <Navigate to="/dashboard" /> : <Navigate to="/login" />} />
-          <Route path="/login" element={!token ? <Login /> : <Navigate to="/dashboard" />} />
-          <Route path="/signup" element={!token ? <Signup /> : <Navigate to="/dashboard" />} />
+          <Route
+            path="/"
+            element={
+              token ? <Navigate to="/dashboard" /> : <Navigate to="/login" />
+            }
+          />
+          <Route
+            path="/login"
+            element={!token ? <Login /> : <Navigate to="/dashboard" />}
+          />
+          <Route
+            path="/signup"
+            element={!token ? <Signup /> : <Navigate to="/dashboard" />}
+          />
 
           {/* Main Routes */}
-          <Route path="/dashboard" element={token ? <Dashboard /> : <Navigate to="/login" />} />
+          <Route
+            path="/dashboard"
+            element={token ? <Dashboard /> : <Navigate to="/login" />}
+          />
 
-          <Route path="/sections" element={token ? <Sections /> : <Navigate to="/login" />} />
-          <Route path="/grades" element={token ? <Grades /> : <Navigate to="/login" />} />
-          <Route path="/account" element={token ? <Account /> : <Navigate to="/login" />} />
+          <Route
+            path="/sections"
+            element={token ? <Rooms /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/grades"
+            element={token ? <Grades /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/account"
+            element={token ? <Account /> : <Navigate to="/login" />}
+          />
           {/* Admin Routes */}
 
-          <Route path="/admin" element={token ? <Admin /> : <Navigate to="/login" />} />
+          <Route
+            path="/admin"
+            element={token ? <Admin /> : <Navigate to="/login" />}
+          />
         </Routes>
-
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
