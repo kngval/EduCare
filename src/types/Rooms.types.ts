0a528cc4f394328e26b30aacfa9f8833c21c6402
kngval <bkvalteocs@gmail.com>
2024-12-09 17:22:05 +0800

    room details structure

--- src/types/Rooms.types.ts
+++ src/types/Rooms.types.ts
@@ -3,3 +3,11 @@ export type RoomType = {
   roomCode: string;
   subjectName: string;
 };
+
+export type TRoomDetails = {
+  id : number,
+  roomCode : string,
+  subjectName : string,
+  teacherId : number | null,
+  teacherName : string | null
+}
