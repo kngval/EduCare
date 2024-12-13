ff52bfae5d64377384fbca225a1d1a1aa4dee872
kngval <bkvalteocs@gmail.com>
2024-12-13 15:19:15 +0800

    rm role state created a usable fn getRole() in => utils

--- src/redux/authSlice.ts
+++ src/redux/authSlice.ts
@@ -1,33 +1,30 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit"
-
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface IAuth {
-  token: string | null
+  token: string | null;
 }
 
-const initialState:IAuth = {
-  token : localStorage.getItem("token"),
-}
-
+const initialState: IAuth = {
+  token: localStorage.getItem("token"),
+};
 
 export const authSlice = createSlice({
   name: "auth",
   initialState,
-  reducers :{
-    setToken : (state,action:PayloadAction<string>) => {
-      state.token = action.payload
-      localStorage.setItem("token",action.payload)
+  reducers: {
+    setToken: (state, action: PayloadAction<string>) => {
+      state.token = action.payload;
+      localStorage.setItem("token", action.payload);
     },
-    logout : (state) => {
+    logout: (state) => {
       state.token = null;
       localStorage.removeItem("token");
-    }
+    },
   },
   //optional builders for later
   // extraReducers(builder) {
   // },
+});
 
-})
-
-export const { setToken,logout } = authSlice.actions;
+export const { setToken, logout } = authSlice.actions;
 export default authSlice.reducer;
